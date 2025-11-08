03b15da9901c00e9f212fd0777d4a5772f0c515e
Natalie <26949776@sun.ac.za>
2025-11-08 14:49:25 +0000

    gg

--- my-admin/src/App.jsx
+++ my-admin/src/App.jsx
@@ -1,13 +1,16 @@
-import { Admin, Resource, ListGuesser, EditGuesser } from "react-admin";
+import { Admin, Resource, ListGuesser, EditGuesser, memoryStore } from "react-admin";
 import { createTrailbaseProvider } from "/workspaces/TAKE-FUCKING-2/my-admin/ra-trailbase.js";
 
-const TRAILBASE_URL = "https://humble-space-engine-q795q76j7q9p3q6w-4000.app.github.dev/";
-const {dataProvider} = createTrailbaseProvider(TRAILBASE_URL);
+const TRAILBASE_URL = "https://humble-space-engine-q795q76j7q9p3q6w-4000.app.github.dev/"
+const {dataProvider} = await createTrailbaseProvider(TRAILBASE_URL);
 
 const App = () =>
    (
-  <Admin dataProvider={dataProvider}>
-    <Resource name="authors" list={AuthorsList} edit={EditGuesser}/>
+  <Admin 
+  store={memoryStore()}
+  dataProvider={dataProvider}
+  >
+    <Resource name="authors" list={ListGuesser} edit={EditGuesser}/>
     <Resource name="books" list={ListGuesser} edit={EditGuesser}/>
     <Resource name="users" list={ListGuesser} edit={EditGuesser}/>
   </Admin>
